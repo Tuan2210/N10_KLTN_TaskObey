3b09dc08c951588714bd730105a43b1760d3b4be
Tuan2210 <quangtuan496>
2023-03-12 22:27:58 +0700

    demo load list not finishTasks & finishTasks

--- task_obey/src/components/screens/FinishTaskScreen.js
+++ task_obey/src/components/screens/FinishTaskScreen.js
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, FlatList } from "react-native";
 import React, { useEffect, useState } from "react";
 import { Link, useNavigate } from "react-router-native";
 import { useDispatch, useSelector } from "react-redux";
@@ -14,8 +14,9 @@ export default function FinishTaskScreen() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
-  const [dataTaskName, setDataTaskName] = useState();
-  const [dataDayTime, setDataDayTime] = useState();
+  const [dataList, setDataList] = useState([]);
+  // const [dataTaskName, setDataTaskName] = useState();
+  // const [dataDayTime, setDataDayTime] = useState();
   useEffect(() => {
     loadListNotFinishTasks(userId);
   }, [userId]);
@@ -23,22 +24,101 @@ export default function FinishTaskScreen() {
   async function loadListNotFinishTasks(userId) {
     await axios.get(`${url}/api/task/finishTasks/${userId}`).then((res) => {
       if (res.data.length > 0) {
-        {
-          res.data.map((taskData, index) => {
-            console.log(taskData);
-            setDataTaskName(taskData.taskName);
-            setDataDayTime(taskData.dayTime);
-          });
-        }
+        // {
+        //   res.data.map((taskData, index) => {
+        //     console.log(taskData);
+        //     setDataTaskName(taskData.taskName);
+        //     setDataDayTime(taskData.dayTime);
+        //   });
+        // }
+        setDataList(res.data);
       }
     });
   }
 
+  let sliceDay, sliceTime;
+  const [time, setTime] = useState();
+  const [selectedIdItemData, setSelectedIdItemData] = useState(null);
+  const ItemData = ({ item, onPress, activeItemBg, activeItemFont }) => (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        activeItemBg,
+        {
+          width: "100%",
+          height: 200,
+          alignSelf: "center",
+          justifyContent: "space-around",
+          // marginLeft: "3%",
+          marginTop: "4%",
+          borderRadius: 20,
+          borderColor: "#09CBD0",
+          borderWidth: 1.2,
+          borderStyle: "solid",
+        },
+      ]}
+    >
+      <View style={{ padding: '5%' }}>
+        <Text 
+          style={[
+            activeItemFont, 
+            { 
+              fontSize: 20, 
+              // backgroundColor:"red", 
+              width: '100%',
+              height: '85%'
+            }]}
+        >
+          {item.taskName}
+        </Text>
+        <Text
+          style={{
+            color: "#D4A055",
+            fontSize: 16,
+            fontWeight: "bold",
+            alignSelf: "flex-end"
+          }}
+        >
+          {
+            [
+              sliceDay = (new Date(item.dayTime)).toLocaleDateString(),', ',
+              sliceTime = (new Date(item.dayTime)).toLocaleTimeString()
+            ]
+          }
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <SafeAreaView>
       <View>
-        <Text>{dataTaskName}</Text>
-        <Text>{dataDayTime}</Text>
+        {/* <Text>{dataTaskName}</Text>
+        <Text>{dataDayTime}</Text> */}
+        <FlatList
+          key={"#"}
+          // numColumns={2}
+          data={dataList}
+          renderItem={({ item }) => {
+            // const backgroundColor =
+            //   item.id === selectedIdItemData ? "#fff" : "black";
+            // const color = item.id === selectedIdItemData ? "black" : "#fff";
+            return (
+              <ItemData
+                item={item}
+                onPress={() => setSelectedIdItemData(item.id)}
+                activeItemBg={{}}
+                activeItemFont={{}}
+                // viewImgItem={{ width: "auto", height: 100 }}
+              />
+            );
+          }}
+          style={{
+            width: "100%",
+            height: "100%",
+            padding: "5%",
+          }}
+        />
       </View>
     </SafeAreaView>
   );
--- task_obey/src/components/screens/ListScreen.js
+++ task_obey/src/components/screens/ListScreen.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useRef, useState } from "react";
-import { Animated, Dimensions, Image, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Animated, Dimensions, FlatList, Image, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 import { Link, useNavigate } from "react-router-native";
 import { useDispatch, useSelector } from "react-redux";
 import axios from "axios";
@@ -21,6 +21,7 @@ export default function ListScreen() {
   const navigate = useNavigate();
   const dispatch = useDispatch();
 
+  const [dataList, setDataList] = useState([]);
   const [dataTaskName, setDataTaskName] = useState();
   const [dataDayTime, setDataDayTime] = useState();
   useEffect(() => {
@@ -32,28 +33,107 @@ export default function ListScreen() {
       .get(`${url}/api/task/notFinishTasks/${userId}`)
       .then((res) => {
         if(res.data.length > 0) {
-          {res.data.map((taskData, index) => {
-            console.log(taskData);
-            setDataTaskName(taskData.taskName);
-            setDataDayTime(taskData.dayTime);
-          })}
+          // {res.data.map((taskData, index) => {
+          //   console.log(taskData);
+          //   setDataTaskName(taskData.taskName);
+          //   setDataDayTime(taskData.dayTime);
+          // })}
+          setDataList(res.data);
         };
       })
   }
 
+  let sliceDay, sliceTime;
+  const [time, setTime] = useState();
+  const [selectedIdItemData, setSelectedIdItemData] = useState(null);
+  const ItemData = ({ item, onPress, activeItemBg, activeItemFont }) => (
+    <TouchableOpacity
+      onPress={onPress}
+      style={[
+        activeItemBg,
+        {
+          width: "100%",
+          height: 200,
+          alignSelf: "center",
+          justifyContent: "space-around",
+          // marginLeft: "3%",
+          marginTop: "4%",
+          borderRadius: 20,
+          borderColor: "#09CBD0",
+          borderWidth: 1.2,
+          borderStyle: "solid",
+        },
+      ]}
+    >
+      <View style={{ padding: '5%' }}>
+        <Text 
+          style={[
+            activeItemFont, 
+            { 
+              fontSize: 20, 
+              // backgroundColor:"red", 
+              width: '100%',
+              height: '85%'
+            }]}
+        >
+          {item.taskName}
+        </Text>
+        <Text
+          style={{
+            color: "#D4A055",
+            fontSize: 16,
+            fontWeight: "bold",
+            alignSelf: "flex-end"
+          }}
+        >
+          {
+            [
+              sliceDay = (new Date(item.dayTime)).toLocaleDateString(),', ',
+              sliceTime = (new Date(item.dayTime)).toLocaleTimeString()
+            ]
+          }
+        </Text>
+      </View>
+    </TouchableOpacity>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <View>
-        <Text>{dataTaskName}</Text>
-        <Text>{dataDayTime}</Text>
+        {/* <Text>{dataTaskName}</Text>
+        <Text>{dataDayTime}</Text> */}
+        <FlatList
+          key={"#"}
+          // numColumns={2}
+          data={dataList}
+          renderItem={({ item }) => {
+            // const backgroundColor =
+            //   item.id === selectedIdItemData ? "#fff" : "black";
+            // const color = item.id === selectedIdItemData ? "black" : "#fff";
+            return (
+              <ItemData
+                item={item}
+                onPress={() => setSelectedIdItemData(item.id)}
+                activeItemBg={{  }}
+                activeItemFont={{  }}
+                // viewImgItem={{ width: "auto", height: 100 }}
+              />
+            );
+          }}
+          style={{
+            width: '100%', 
+            height: '100%',
+            padding: '5%'
+          }}
+        />
       </View>
     </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff'
-    }
-})
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+});
