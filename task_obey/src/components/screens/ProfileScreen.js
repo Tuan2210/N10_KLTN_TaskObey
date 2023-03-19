c729d304c3b3e248fca44739cec945fcd6b2513e
stoky71 <81395239+stoky71@users.noreply.github.com>
2023-03-19 12:34:10 +0700

    update pro5 screen and fix responsive

--- task_obey/src/components/screens/ProfileScreen.js
+++ task_obey/src/components/screens/ProfileScreen.js
@@ -9,6 +9,7 @@ import { logOut, logOutRegsiter } from "../../redux/apiRequest/authApiRequest";
 
 import { SafeAreaView } from "react-native-safe-area-context";
 
+import {Avatar, Drawer} from 'react-native-paper'
 //link all name animations: https://github.com/oblador/react-native-animatable
 //link how to code animation: https://blog.bitsrc.io/top-5-animation-libraries-in-react-native-d00ec8ddfc8d
 import * as Animatable from "react-native-animatable";
@@ -53,10 +54,36 @@ export default function ProfileScreen() {
 
   return (
     <SafeAreaView style={styles.container}>
-      <Text>profile</Text>
-      <TouchableOpacity style={styles.btns} onPress={handleLogout}>
-        <Text style={styles.labelBtns}>Đăng xuất</Text>
-      </TouchableOpacity>
+      {/* <Text>profile</Text> */}
+      <View style={{width: "100%", height:"20%"}}>
+          <Avatar.Image source={require("../../../assets/avaUser.gif")} 
+            size={200} 
+            style={styles.avatar}/>
+      </View>
+      <View style={styles.form}>
+        <Text style={styles.tittle}>Thông tin cá nhân</Text>
+        <View style={styles.infoRow}>
+          {/* <Text style={styles.tittleInfo}>Tên tài khoản: </Text> */}
+          <TextInput editable={false} placeholder="Tên tài khoản" style={styles.StyleTextInput}></TextInput>
+        </View>
+        <View style={styles.infoRow}>
+          {/* <Text style={styles.tittleInfo}>Số điện thoại: </Text> */}
+          <TextInput editable={false} placeholder="Số điện thoại" style={styles.StyleTextInput}></TextInput>
+        </View>
+        <View style={styles.infoRow}>
+          {/* <Text style={styles.tittleInfo}>Mật khẩu: </Text> */}
+          <TextInput editable={false} placeholder="Mật khẩu" style={styles.StyleTextInput}></TextInput>
+        </View>
+        <View style={styles.columnButton}>
+        <TouchableOpacity style={styles.btns} onPress={handleLogout}>
+          <Text style={styles.tittleLabel}>Cập nhật thông tin</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btns} onPress={handleLogout}>
+          <Text style={styles.tittleLabel}>Đổi mật khẩu</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+      
     </SafeAreaView>
   );
 };
@@ -67,12 +94,80 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
   },
   btns: {
-    padding: 15,
+    // padding: 15,
     backgroundColor: "#09CBD0",
     borderRadius: 100,
-    width: "40%",
+    width: "70%",
+    height: "40%",
     flexDirection: "row",
     justifyContent: "center",
     alignItems: "center",
+    margin: "5%",
+    alignSelf: 'center'
+  },
+  box: {
+    marginTop: '5%',
+    height: '100%',
+    backgroundColor: "#fff",
+    borderRadius: 80,
+    justifyContent: "center"
+  },
+  form: {
+    flex: 0.99,
+    alignSelf: "center",
+  },
+  tittle: {
+    flex: 0.11,
+    fontSize: 23,
+    margin: "5%",
+    fontWeight:'700',
+    textAlign:'center',
+    color:"#09CBD0",
+  },
+  avatar:{
+    marginTop: "-7%",
+    height: "25%",
+    width: "50%",
+    alignSelf:'center'
+  },
+  tittleInfo: {
+    textAlign:"left",
+    color:"#09CBD0",  
+    fontWeight: '500',
+    fontSize:17,
+    width: "40%",
+    marginTop: "-0.25%"
+  },
+  infoRow: {
+    flex: 0.1099,
+    flexDirection:'row',
+    justifyContent: "space-around",
+    margin: "4.8%",
+    height: "35%"
+  },
+  columnButton: {
+    flex: 0.4,
+    flexDirection: 'column',
+    margin: "5%"
+  },
+  StyleTextInput: {
+    backgroundColor: "rgba(211, 211, 211, 0.404)",
+    alignSelf: "center",
+    width: "95%",
+    height: "127%",
+    fontSize: 18,
+    marginTop: 5,
+    marginRight: 0,
+    marginBottom: 5,
+    marginLeft: 0,
+    paddingLeft: 10,
+    borderRadius: 15,
+    borderColor: 'black',
+    borderWidth:1,
+  },
+  tittleLabel: {
+    fontSize: 20,
+    fontWeight: '700',
+    color: '#fff'
   },
 });
