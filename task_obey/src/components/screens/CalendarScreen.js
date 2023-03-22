76a2f1257e9daef25d49d8e004afe1cb6205068a
stoky71 <81395239+stoky71@users.noreply.github.com>
2023-03-22 11:14:23 +0700

    update vietsub calendar and background color today

--- task_obey/src/components/screens/CalendarScreen.js
+++ task_obey/src/components/screens/CalendarScreen.js
@@ -2,11 +2,48 @@ import React, { useEffect, useRef, useState } from "react";
 import { Animated, Dimensions, Image, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Calendar } from 'react-native-calendars';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
 // link calendar: https://github.com/wix/react-native-calendars
 //link all name animations: https://github.com/oblador/react-native-animatable
 //link how to code animation: https://blog.bitsrc.io/top-5-animation-libraries-in-react-native-d00ec8ddfc8d
 import * as Animatable from "react-native-animatable";
+
+LocaleConfig.locales['vn'] = {
+    monthNames: [
+        "Tháng một",
+        "Tháng hai",
+        "Tháng ba",
+        "Tháng bốn",
+        "Tháng năm",
+        "Tháng sáu",
+        "Tháng bảy",
+        "Tháng tám",
+        "Tháng chín",
+        "Tháng mười",
+        "Tháng mười một",
+        "Tháng mười hai"
+    ],
+    dayNames: [
+        "Thứ hai",
+        "Thứ ba",
+        "Thứ tư",
+        "Thứ năm",
+        "Thứ sáu",
+        "Thứ bảy",
+        "Chủ nhật"
+    ],
+    dayNamesShort: [
+        "Hai",
+        "Ba",
+        "Tư",
+        "Năm",
+        "Sáu",
+        "Bảy",
+        "CN"
+    ],
+}
+
+LocaleConfig.defaultLocale = 'vn'
 
 const widthScreen = Dimensions.get("window").width;
 const heightScreen = Dimensions.get("window").height;
@@ -14,7 +51,21 @@ const heightScreen = Dimensions.get("window").height;
 export default function CalendarScreen() {
     return (
         <SafeAreaView style={styles.container}>
-            <Calendar style={{height:'100%', width: '100%',alignContent:'center'}}/>
+            <Calendar style={{height:'100%', width: '100%',alignContent:'center'}}
+                theme={{
+                    backgroundColor: "#fff",
+                    textSectionTitleColor: "#09CBD0",
+                    selectedDayBackgroundColor: "#09CBD0",
+                    selectedDayTextColor:"#fff",
+                    todayTextColor: "#fff",
+                    selectedDotColor: "#09CBD0",
+                    todayDotColor:"#09CBD0",
+                    dotColor: "#09CBD0",
+                    todayBackgroundColor:"#09CBD0",
+                    agendaTodayColor:"#09CBD0",
+                    agendaDayNumColor:"#09CBD0",
+                    agendaDayTextColor:"#fff",
+                }}/>
         </SafeAreaView>
     )
 };
