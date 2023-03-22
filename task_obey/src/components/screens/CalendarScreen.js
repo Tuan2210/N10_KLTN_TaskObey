999781600c0e9ed9172b47f13fc7c2b183ebceb8
stoky71 <81395239+stoky71@users.noreply.github.com>
2023-03-22 14:40:34 +0700

    fix name days in calendar screen

--- task_obey/src/components/screens/CalendarScreen.js
+++ task_obey/src/components/screens/CalendarScreen.js
@@ -34,12 +34,12 @@ LocaleConfig.locales['vn'] = {
     ],
     dayNamesShort: [
         "CN",
-        "Hai",
-        "Ba",
-        "Tư",
-        "Năm",
-        "Sáu",
-        "Bảy",
+        "T2",
+        "T3",
+        "T4",
+        "T5",
+        "T6",
+        "T7",
     ],
 }
 
