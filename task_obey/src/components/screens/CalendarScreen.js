ba9b20abe84977b6e6f3288fa3067c2d692da8b7
stoky71 <81395239+stoky71@users.noreply.github.com>
2023-03-30 11:53:43 +0700

    update calendar screen and fix something

--- task_obey/src/components/screens/CalendarScreen.js
+++ task_obey/src/components/screens/CalendarScreen.js
@@ -2,7 +2,7 @@ import React, { useEffect, useMemo, useRef, useState } from "react";
 import { Animated, Dimensions, Image, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 import { SafeAreaView } from "react-native-safe-area-context";
-import { Calendar,CalendarList, LocaleConfig } from 'react-native-calendars';
+import { CalendarList, LocaleConfig } from 'react-native-calendars';
 // link calendar: https://github.com/wix/react-native-calendars
 //link all name animations: https://github.com/oblador/react-native-animatable
 //link how to code animation: https://blog.bitsrc.io/top-5-animation-libraries-in-react-native-d00ec8ddfc8d
@@ -23,40 +23,27 @@ LocaleConfig.locales['vn'] = {
         "Tháng mười một",
         "Tháng mười hai"
     ],
-    dayNames: [
-        "Thứ hai",
-        "Thứ ba",
-        "Thứ tư",
-        "Thứ năm",
-        "Thứ sáu",
-        "Thứ bảy",
-        "Chủ nhật"
-    ],
-    dayNamesShort: [
-        "CN",
-        "T2",
-        "T3",
-        "T4",
-        "T5",
-        "T6",
-        "T7",
-    ],
+    monthNamesShort:["T1","T2","T3","T4","T5","T7","T8","T9","T10","T11","T12", ],
+    dayNames: [ "Thứ hai", "Thứ ba", "Thứ tư", "Thứ năm", "Thứ sáu", "Thứ bảy", "Chủ nhật"],
+    dayNamesShort: [ "CN", "T2", "T3", "T4", "T5", "T6", "T7",]
 }
 
 LocaleConfig.defaultLocale = 'vn'
 
-const widthScreen = Dimensions.get("window").width;
-const heightScreen = Dimensions.get("window").height;
-
 export default function CalendarScreen(props) {
-    const initDate = '2022-12-01';
-    const [selected, setSelected] = useState(initDate);
+    const presentDate = new Date().toISOString().split("T")[0];
+    const presentDay = presentDate.slice(8,10),
+            presentMonth = presentDate.slice(5,7),
+            presentYear = presentDate.slice(0,4),
+            formatpresentDate = presentYear +'-' +presentMonth + '-'  +presentDay ;
+    const [selected, setSelected] = useState(formatpresentDate);
     const marked = useMemo(() => ({
         [selected]: {
+        // selected: true,
         customStyles: {
             container: {
             borderColor: "#09CBD0",
-            borderWidth: 1.5,
+            borderWidth: 1.6,
             borderRadius: 100,
             },
             text: {
@@ -69,26 +56,19 @@ export default function CalendarScreen(props) {
         <SafeAreaView style={styles.container}>
             <CalendarList style={{height:'100%', width: '100%',alignContent:'center'}}
                 theme={{
-                    backgroundColor: "#fff",
-                    textSectionTitleColor: "#09CBD0",
-                    selectedDayBackgroundColor: "#09CBD0",
                     selectedDayTextColor:"#fff",
                     todayTextColor: "#fff",
-                    selectedDotColor: "#09CBD0",
-                    todayDotColor:"#09CBD0",
-                    dotColor: "#09CBD0",
                     todayBackgroundColor:"#09CBD0",
-                    agendaTodayColor:"#09CBD0",
-                    agendaDayNumColor:"#09CBD0",
-                    agendaDayTextColor:"#fff",
                 }}
+                initialDate={formatpresentDate}
                 markingType="custom"
                 markedDates={marked}
                 onDayPress={(day) => {
                     setSelected(day.dateString);
                     props.onDaySelect && props.onDaySelect(day);
                 }}
-                {...props}/>
+                {...props}
+                />
         </SafeAreaView>
     )
 };
