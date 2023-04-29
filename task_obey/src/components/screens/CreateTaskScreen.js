57ef3fbfd61c59ed0cf4ff3d6057597a0c83f33d
Đinh Quang Tuấn <quangtuan496@gmail.com>
2023-04-29 12:40:03 +0700

    fix bug

--- task_obey/src/components/screens/CreateTaskScreen.js
+++ task_obey/src/components/screens/CreateTaskScreen.js
@@ -101,7 +101,7 @@ export default function CreateTaskScreen() {
       setShowStartDateTime(false);
       setModeStartDateTime("");
       console.log(startDate);
-      console.log('pick startDateTime:', displayStartDate +",\u00A0" +displayStartTime);
+      console.log('pick startDateTime:', fDate1 +",\u00A0" +fTime1);
     }
   }
   //////
@@ -146,7 +146,7 @@ export default function CreateTaskScreen() {
         setShowEndDateTime(false);
         setModeEndDateTime("");
         console.log(endDate);
-        console.log('pick endDateTime:', endDateTime);
+        console.log('pick endDateTime:', fDate2 +",\u00A0" +fTime2);
       }
     } else {
       setDisplayEndDate("... / ... / ....");
@@ -386,7 +386,7 @@ export default function CreateTaskScreen() {
       description: txtInputDesc,
       priority: priority,
       startTime: displayStartDate +', ' +displayStartTime,
-      endTime: endDateTime,
+      endTime: displayEndDate +', ' +displayEndTime,
       reminderTime: reminderTime,
       duration: duration,
       deadline: deadline,
