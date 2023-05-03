37587246471c539938c7c59b39997aaaee4e0e15
Đinh Quang Tuấn <quangtuan496@gmail.com>
2023-05-03 10:12:00 +0700

    increase sv size

--- server/index.js
+++ server/index.js
@@ -1,12 +1,12 @@
-const express = require('express');
+const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
-const http = require('http');
+const http = require("http");
 
 const app = express();
 
-const authRoute = require('./routes/auth');
+const authRoute = require("./routes/auth");
 const userRoute = require("./routes/user");
 const taskRoute = require("./routes/task");
 
@@ -18,20 +18,22 @@ const port = process.env.PORT || 8000;
 dotenv.config();
 //CONNECT MONGODB
 mongoose.connect(process.env.MONGODB_URL, () => {
-	console.log('Connect to MongoDB');
+  console.log("Connect to MongoDB");
 });
 
-app.use(express.json());
+// app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(
   cors({
     credentials: true,
-    origin: 'http://localhost:3000',
+    origin: "http://localhost:3000",
   })
 );
 
-app.use('/api/auth', authRoute);
-app.use('/api/user', userRoute);
-app.use('/api/task', taskRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/user", userRoute);
+app.use("/api/task", taskRoute);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log(`Server started on port ${port}`));
