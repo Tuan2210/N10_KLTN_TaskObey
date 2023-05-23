85d06919fc919d6d217104c8861412a0efb52911
Đinh Quang Tuấn <quangtuan496@gmail.com>
2023-05-23 22:24:13 +0700

    add res status update-task

--- server/controllers/taskController.js
+++ server/controllers/taskController.js
@@ -265,6 +265,7 @@ const taskController = {
         { repeat: repeat },
         { new: true }
       ).then((updated) => console.log(updated));
+      res.status(200).json({ success: true, message: "Update successfully!" })
     } catch (error) {
       res.status(500).json(error);
     }
